efe0fbd905dc62451a031c1ff4eced861e4394b9
Richard Musime <richardmusime6@gmail.com>
2024-12-20 12:40:40 +0200

    added the workers management

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,9 +1,20 @@
-sap.ui.define(["./BaseController", "sap/m/MessageBox"], function (BaseController, MessageBox) {
-	"use strict";
+sap.ui.define([
+    "./BaseController",
+    "sap/m/MessageBox",
+    "sap/ui/model/odata/v4/ODataModel"
+], function (BaseController, MessageBox, ODataModel) {
+    "use strict";
 
-	return BaseController.extend("workersWebsite.controller.Main", {
-		sayHello: function () {
-			MessageBox.show("Hello World!");
-		}
-	});
+    return BaseController.extend("workersWebsite.controller.Main", {
+        onInit: function () {
+            const oModel = new ODataModel({
+                serviceUrl: "http://localhost:4000/odata/",
+                synchronizationMode: "None",
+               
+            });
+            
+            this.getView().setModel(oModel);
+                       
+        }
+    });
 });
